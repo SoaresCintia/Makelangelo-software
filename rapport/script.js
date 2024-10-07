b1cc44385732048a1fd4252a00e638d62a2aaaf7
Cíntia Dalila Soares <cintiasoares@gmail.com>
2024-10-07 15:36:26 -0400

    Organisation des fichiers

---
+++ rapport/script.js
@@ -0,0 +1,4 @@
+// Script JS pour afficher une alerte à l'ouverture de la page
+window.onload = function () {
+    alert("Bienvenue dans le rapport sur l'ajout de tests dans Makelangelo-software !");
+};
